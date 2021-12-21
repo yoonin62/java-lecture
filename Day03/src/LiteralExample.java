0e47bb973911362b114b7dd1fa06ec420da9bf9c
yoonin62 <62sehun@naver.com>
2021-12-21 21:21:46 +0900

    리터럴 연습

---
+++ Day03/src/LiteralExample.java
@@ -0,0 +1,14 @@
+
+public class LiteralExample {
+
+	public static void main(String[] args) {
+		
+		//리터럴(이스케이프 시퀀스)
+		String desc = "\"오잉\"\n 고고씽\n 홈 \t으로";
+		System.out.println(desc);
+		
+		
+		
+	}
+	
+}
