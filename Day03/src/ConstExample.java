d958b0e899e1c092a04556eac955d1ddb3ab4378
yoonin62 <62sehun@naver.com>
2021-12-21 21:10:58 +0900

    상수연습

---
+++ Day03/src/ConstExample.java
@@ -0,0 +1,25 @@
+
+public class ConstExample {
+
+	public static void main(String[] args) {
+
+		//변수란 변할 수 있는 수
+		//상수란 변할 수 없는 수
+		
+		int age = 10;
+		System.out.println(age);
+		
+		age = 20;
+		System.out.println(age);
+		
+		//상수는 final 키워드 사용
+		//final 데이터타입(자료형) 상수명 = 값
+		double PI = 3.14;
+		System.out.println(PI);
+		
+		PI = 3.20;
+		System.out.println(PI);
+
+	}
+
+}
